9571683f13c13b65dfd490a8949518dd70261d18
sebaslopezjuarez <69880486+sebaslopezjuarez@users.noreply.github.com>
2020-08-25 09:59:03 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Sebastian Lopez Juarez
+ *         slopezjuarez2@unl.edu
+ * Date: 2020/17/08
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+    printf("Sebastian Lopez Juarez\n");
+
+    return 0;
+}
